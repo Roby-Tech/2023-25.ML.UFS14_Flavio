2301e0ca778c8d7fb630b50dc9884aefed3fe4ca
Roby <roberto.livrieri@itsrizzoli.it>
2024-10-18 13:45:55 +0000

    aggiornamento

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ import os
 model_dir = os.environ['SM_MODEL_DIR']
 
 with open(model_dir + '/output.txt', 'w') as f:
-    f.write('Ciao')
+    f.write('Ciao sono il modello di ml')
     
 output_dir = os.environ['SM_OUTPUT_DIR']
 with open(output_dir + '/output.txt', 'w') as f:
-    f.write('Ciao')
+    f.write('Ciao sono i log del training')
+
+input_dir = os.environ['SM_OUTPUT_DIR']
+with open(input_dir + '/train.csv', 'r') as fp:
+    lines = len(fp.readlines())
+    print('######Total Number of lines', lines)
